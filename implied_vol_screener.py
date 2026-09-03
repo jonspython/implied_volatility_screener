21857db70932b8261c17f3cba940a35f6934ce38
jonspython <jonathan@hysharpe.com>
2026-09-03 15:18:02 -0700

    Revert "Add Adjusted Annualized Yield calculation and formatting"
    This reverts commit bcdfaa0dc1573a399d9a9b1cd55efd07751765d2.

--- implied_vol_screener.py
+++ implied_vol_screener.py
@@ -149,17 +149,6 @@ def get_option_statistics(ticker, current_price):
             365/dte
         )
 
-    adjusted_premium = bid + (current_price - strike)
-    adjusted_collateral = current_price - adjusted_premium
-    adjusted_annualized_yield = np.nan
-    
-    if adjusted_collateral > 0:
-        adjusted_annualized_yield = (
-            adjusted_premium /
-            adjusted_collateral *
-            365/dte
-        )
-
     spread_pct = np.nan
 
     if midpoint>0:
@@ -196,9 +185,6 @@ def get_option_statistics(ticker, current_price):
 
         "Annualized Yield":
             annualized_yield,
-
-        "Adjusted Annualized Yield":
-            adjusted_annualized_yield,
 
         "Bid/Ask Spread %":
             spread_pct,
@@ -352,7 +338,7 @@ def option_screen(ticker_list, max_workers=2):
         "10-Day (H-L)/C", "30-Day (H-L)/C", "10-Day / 30-Day (H-L)/C",
         "3-Year Return", "Expiration", "DTE", "Strike",
         "Bid", "Ask", "Midpoint", "Premium %", "OTM %", "Annualized Yield",
-        "Adjusted Annualized Yield", "Bid/Ask Spread %", "Option Volume", "Open Interest"
+        "Bid/Ask Spread %", "Option Volume", "Open Interest"
     ]
 
     columns = [c for c in columns if c in df.columns]
